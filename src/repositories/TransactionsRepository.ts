b1ac2f85c942fda882b8b243baeb5798715579ed
marcosvella <62914841+marcosvella@users.noreply.github.com>
2020-12-01 22:14:25 -0300

    Challenge 5 - sucess

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.map(transaction => {
+      if (transaction.type === 'income') {
+        balance.income += transaction.value;
+      }
+      if (transaction.type === 'outcome') {
+        balance.outcome += transaction.value;
+      }
+      return transaction;
+    });
+
+    balance.total = balance.income - balance.outcome;
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const balance = this.getBalance();
+
+    if (balance.total < value && type === 'outcome') {
+      throw Error('Could not withdraw because total would be negative');
+    }
+
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
